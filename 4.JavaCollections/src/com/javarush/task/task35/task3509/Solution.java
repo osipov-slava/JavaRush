42af51300b78c3a1b8487721a3b4dea16edb0ab5
Slava Osipov <skybright@mail.ru>
2019-09-27 22:39:29 +0300

    +35lvl

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.*;
+
+
+/* 
+Collections & Generics
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        ArrayList<T> arrayList = new ArrayList<>();
+        for (T t: elements) {
+            arrayList.add(t);
+        }
+        return arrayList;
+    }
+
+    public static <T> HashSet<T> newHashSet(T... elements) {
+        HashSet<T> hashSet = new HashSet<>();
+        for (T t: elements) {
+            hashSet.add(t);
+        }
+        return hashSet;
+    }
+
+    public static <K, V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
+        HashMap<K, V> hashMap = new HashMap<>();
+        if (keys.size() != values.size()) throw new IllegalArgumentException();
+        for (int i = 0; i < keys.size(); i++) {
+            hashMap.put(keys.get(i), values.get(i));
+        }
+        return hashMap;
+    }
+}
